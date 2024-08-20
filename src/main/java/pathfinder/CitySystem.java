014face5ce3ad281dbed1dcac7fb1fb49d59aa94
shahab <shhm3834@gmail.com>
2024-08-21 02:25:25 +0330

    Add command line UI to City System

--- src/main/java/pathfinder/CitySystem.java
+++ src/main/java/pathfinder/CitySystem.java
@@ -3,6 +3,8 @@ package pathfinder;
 import graph.Graph;
 import graph.Node;
 
+import java.util.Scanner;
+
 public class CitySystem {
     private final Graph graph;
     private final Governor governor;
@@ -51,5 +53,58 @@ public class CitySystem {
         graph.bfs(startCity);
         avoidCity.setAvoid(false);
         return endCity.isVisited();
+    }
+
+    public void processRequests() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String request = scanner.nextLine();
+            switch (request) {
+                case "one-way":
+                    makeRoutesOneWay();
+                    break;
+                case "two-way":
+                    makeRoutesTwoWay();
+                    break;
+                case "change-train-time":
+                    int newTime = scanner.nextInt();
+                    changeTrainTime(newTime);
+                    break;
+                case "train-time":
+                    int startIndex = scanner.nextInt();
+                    int endIndex = scanner.nextInt();
+                    Node startCity = graph.getGraph().get(startIndex - 1);
+                    Node endCity = graph.getGraph().get(endIndex - 1);
+                    System.out.println("Train time: " + calculateTrainTime(startCity, endCity));
+                    break;
+                case "bus-time":
+                    startIndex = scanner.nextInt();
+                    endIndex = scanner.nextInt();
+                    startCity = graph.getGraph().get(startIndex - 1);
+                    endCity = graph.getGraph().get(endIndex - 1);
+                    System.out.println("Bus time: " + calculateBusTime(startCity, endCity));
+                    break;
+                case "fastest-time":
+                    startIndex = scanner.nextInt();
+                    endIndex = scanner.nextInt();
+                    startCity = graph.getGraph().get(startIndex - 1);
+                    endCity = graph.getGraph().get(endIndex - 1);
+                    System.out.println("Fastest transport is : " + getFastestTransport(startCity, endCity));
+                    break;
+                case "avoid-city":
+                    startIndex = scanner.nextInt();
+                    endIndex = scanner.nextInt();
+                    int avoidIndex = scanner.nextInt();
+                    startCity = graph.getGraph().get(startIndex - 1);
+                    endCity = graph.getGraph().get(endIndex - 1);
+                    Node avoidCity = graph.getGraph().get(avoidIndex - 1);
+                    System.out.println("Can travel without visiting city " + avoidIndex + ": " + canAvoidCity(startCity, endCity, avoidCity));
+                    break;
+                case "exit":
+                    return;
+                default:
+                    System.out.println("Invalid request.");
+            }
+        }
     }
 }
